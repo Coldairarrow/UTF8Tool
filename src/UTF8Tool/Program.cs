807d6e1a8b8eed3b7b78a721914113192f305602
Coldairarrow <xgc15657519397@outlook.com>
2020-01-07 16:42:55 +0800

    更新

--- src/UTF8Tool/Program.cs
+++ src/UTF8Tool/Program.cs
@@ -9,17 +9,16 @@ namespace UTF8Tool
     {
         static void Main(string[] args)
         {
-            string path = $"D:\\文档\\0软件项目\\GitHub\\Colder.Admin.AntdVue";
-            var files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
-            foreach(var aFile in files)
-            {
-                var oldEncoding = EncodingHelper.GetEncodingString(new FileInfo(aFile));
-                var content = File.ReadAllText(aFile, Encoding.GetEncoding(oldEncoding));
-                File.WriteAllText(aFile, content, Encoding.UTF8);
-            }
+            //string path = $"D:\\文档\\0软件项目\\GitHub\\Colder.Admin.AntdVue";
+            //var files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
+            //foreach(var aFile in files)
+            //{
+            //    var oldEncoding = EncodingHelper.GetEncodingString(new FileInfo(aFile));
+            //    var content = File.ReadAllText(aFile, Encoding.GetEncoding(oldEncoding));
+            //    File.WriteAllText(aFile, content, Encoding.UTF8);
+            //}
 
             Console.WriteLine("完成");
-
         }
     }
 }
